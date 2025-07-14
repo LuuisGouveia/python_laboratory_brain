f2b8d5f005b1c3a9422b858bb44af4ecd9ab629f
Luís Gouveia <luisgouveia74@gmail.com>
2025-07-14 11:44:46 -0300

    configuração basica do front end feita, alternancia de paginas do conteudo principal funcionando, ainda sem implementacao nas paginas html

---
+++ src/laboratory_brain/ui/templates/renderer.js
@@ -0,0 +1,46 @@
+
+
+const content = document.getElementById("content");
+
+const works_btn = document.getElementById("works");
+works_btn.addEventListener("click", ()=>{
+  load_content("./views/works.html");
+})
+
+const clients_btn = document.getElementById("clients");
+clients_btn.addEventListener("click", ()=>{
+  load_content("./views/clients.html");
+})
+
+const prices_btn = document.getElementById("prices");
+prices_btn.addEventListener("click", ()=>{
+  load_content("./views/prices.html");
+})
+
+const notes_btn = document.getElementById("notes");
+notes_btn.addEventListener("click", ()=>{
+  load_content("./views/notes.html");
+})
+
+const work_types_btn = document.getElementById("work_types");
+work_types_btn.addEventListener("click", () => {
+  load_content("./views/work_types.html");
+})
+
+
+function load_content(html_path){
+  fetch(html_path)
+    .then(response => {
+      if (!response.ok){
+        throw new Error("Erro ao carregar a pagina" + response.status);
+      }
+      return response.text();
+    })
+    .then(html => {
+      content.innerHTML=html;
+    })
+    .catch(error => {
+      console.log(error);
+      content.innerHTML='<p>Erro ao carregar a pagina!</p>';
+    });
+}
